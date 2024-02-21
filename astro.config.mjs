e7397ed6e478057c6b8472a4b1f5894aa565becc
Machiel van der Werff <git@machiel.net>
2024-02-21 20:12:11 +0100

    removed tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import bookshop from '@bookshop/astro-bookshop';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://muggy-fish.cloudvent.net/',
-  integrations: [react(), tailwind(), bookshop()],
+  site: 'https://squintconsulting.com/',
+  integrations: [react(), bookshop()],
 });
